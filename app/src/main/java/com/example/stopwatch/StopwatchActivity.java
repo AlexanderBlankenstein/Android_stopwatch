a210bc450cc8248332b49a3ed96513a54ed545da
Alexander Blankenstein <a.blankenstein@live.com>
2022-08-15 19:44:11 -0400

    set up activity's java file with comments and TODOs.

--- app/src/main/java/com/example/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/example/stopwatch/StopwatchActivity.java
@@ -1,14 +1,25 @@
 package com.example.stopwatch;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class StopwatchActivity extends AppCompatActivity {
+public class StopwatchActivity extends Activity {
+
+    private int seconds = 0;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
     }
+
+    //Start the stopwatch running when the Start button is clicked.
+    //TODO: create onClick method for Start and set running.
+
+    //Stop the stopwatch running when the Stop button is clicked
+    //TODO: create onClick method for Stop and set running.
+
+    //Reset the stopwatch when the Reset button
+    //TODO: create onClick method for reset and set running and seconds.
 }
