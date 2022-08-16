505cb99601bd275b4c0c104de976bfb527002f7f
Alexander Blankenstein <a.blankenstein@live.com>
2022-08-15 20:13:09 -0400

    implement runTimer method to update seconds on timer

--- app/src/main/java/com/example/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/example/stopwatch/StopwatchActivity.java
@@ -2,17 +2,24 @@ package com.example.stopwatch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
+import java.util.Locale;
 
 public class StopwatchActivity extends Activity {
 
+    //number of seconds displayed on the stopwatch
     private int seconds = 0;
+    //is the stopwatch running?
     private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+
+        runTimer();
     }
 
     //Start the stopwatch running when the Start button is clicked.
@@ -29,5 +36,30 @@ public class StopwatchActivity extends Activity {
     public void onClickReset(View view) {
         running = false;
         seconds = 0;
+    }
+
+    //Sets the number of seconds on the timer
+    private void runTimer() {
+        final TextView timeView = findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+
+        //Update the stopwatch using a handler with a delay of 1 second
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+
+                //delay post so that display only changes after a second.
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
